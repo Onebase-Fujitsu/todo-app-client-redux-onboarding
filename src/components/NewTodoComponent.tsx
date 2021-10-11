765c30401b6512d6a906eef65bb382a1c416f5b8
onebase <toast20191106@gmail.com>
2021-10-11 14:18:22 +0900

    apply design

--- src/components/NewTodoComponent.tsx
+++ src/components/NewTodoComponent.tsx
@@ -22,7 +22,7 @@ const NewTodoComponent = () => {
         type="text"
         value={input}
         onChange={(event) => setInput(event.target.value)}
-        placeholder="タスクのタイトル"
+        placeholder="タイトル"
         className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full mb-2"
       />
       <button
